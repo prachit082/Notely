62452374a541fd2a5ff846221545e188ecc6bc43
prachit082 <20scs047.prachit.16@gmail.com>
2025-06-04 12:18:47 +0530

    Change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
 export default function Footer() {
-    return (
-      <footer className="w-full text-center py-4 text-sm text-gray-500 dark:text-gray-400">
-        © {new Date().getFullYear()}{" "}
-        <a
-          href="https://github.com/prachit082"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline hover:text-blue-500 transition-colors"
-        >
-          Prachit Pandit
-        </a>. All rights reserved.
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="w-full text-center py-4 text-sm text-gray-500 dark:text-gray-400">
+      NOTELY © {new Date().getFullYear()}{" "}
+      <a
+        href="https://github.com/prachit082"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline hover:text-blue-500 transition-colors"
+      >
+        Prachit Pandit
+      </a>
+      . All rights reserved.
+    </footer>
+  );
+}
